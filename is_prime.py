41519e40ef4780477421740d2d35850eb5979d14
nimafanniasl <76901932+nimafanniasl@users.noreply.github.com>
2021-10-26 13:54:29 +0330

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -2,15 +2,7 @@
 
 import math
 from time import sleep
-
-def yrange(max, min = None):
-    if min != None:
-        num = min
-    else:
-        num = 0
-    while (num<max):
-        yield num
-        num += 1
+from yrange import yrange  #https://github.com/nimafanniasl/py_yrange
 
 def is_prime(num):
     if num == 1:
